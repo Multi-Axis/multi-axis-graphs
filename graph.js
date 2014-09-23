abe4b1e8d365ba4f7614ddc93f02d358709280c6
Antti <antti.karikoski@cs.helsinki.fi>
2014-09-23 13:40:04 +0300

    better parametrisation of graphs

--- graph.js
+++ graph.js
@@ -1,38 +1,57 @@
-var allText = $.getJSON("http://85.23.130.197:8080/data_sample.txt", function(data) {
+var allText = $.getJSON("http://localhost:8080/data_sample.txt", function(data) {
 	draw(data);
 });
 
 function draw(chartData) {
-    var width = 800;
+    var width = 900;
+    var height = 700;
+    var margin = {"left": 50, "right":20, "top": 30, "bottom":30};    
+
+    var xMin = d3.min(chartData, function(d){return Math.min(d.time)});
+    var xMax = d3.max(chartData, function(d){return Math.max(d.time)});
+
+    var yMin = d3.min(chartData, function(d){return Math.min(d.val)});
+    var yMax = d3.max(chartData, function(d){return Math.max(d.val)});
+
+    // console.log(" yMin: " + yMin + " ymax: " + yMax);
+
+    var xScale = d3.time.scale().domain([xMin, xMax]).range([margin.left, width - margin.right]);
+
+    var xAxisScale = d3.time.scale().domain([xMin,xMax]).range([0,width]);
+    var yScale = d3.scale.linear().domain([yMax,1]).range([10,height+margin.top])
+
+    var valueFormat = (function(d) {return yScale(d.val)});
+
     var lineFunc = d3.svg.line()
-                        .x(function(d, i) {return i * (width / chartData.length) + 30 })
-                        .y(function(d) {return width - ((d.val/10000000)*8 - 30)})
+                        .x(function(d, i) {return xScale(d.time)})
+                        .y(valueFormat)
                         .interpolate("none");
 
-    var svgContainer = d3.select("body").append("svg:svg").attr("width", width).attr("height", 810).append("svg:g");
+    var svgContainer = d3.select("body").append("svg:svg").attr("width", width).attr("height", height).append("svg:g");
 
     
     svgContainer.append("svg:path").attr("d",lineFunc(chartData)).attr("stroke","blue").attr("fill","none");
 
-    var axisScaleX = d3.scale.linear().domain([0,10]).range([20,780]);
-    var axisScaleY = d3.scale.linear().domain([100,0]).range([20,780])
 
     var chartXAxis = d3.svg.axis()
-                          .scale(axisScaleX)
+                          .scale(xAxisScale)
                           .orient("bottom")
-                          .ticks(5);
+                          .ticks(10)
+                          .tickFormat(function(d) { return d3.time.format('%b %d, %H : %M')(new Date(d)); });
+
 
     var chartYAxis = d3.svg.axis()
-                          .scale(axisScaleY)
+                          .scale(yScale)
                           .orient("left")
-                          .ticks(5);
+                          .ticks(7)
+                          .tickFormat(function(d) {return (d/1000000) + " MB"});
 
     svgContainer.append("g")
                 .attr("class", "axis")
-                .attr("transform", "translate(30,0)")
+                .attr("transform", "translate("+ margin.left +",0)")
                 .call(chartYAxis);
     svgContainer.append("g")
                 .attr("class", "axis")
-                .attr("transform", "translate(0," + (width-30) + ")")
+                .attr("transform", "translate(0," + (height-margin.bottom) + ")")
                 .call(chartXAxis);
 }
