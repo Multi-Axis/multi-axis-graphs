6ef8f33d13bdc2895a2416c36f85a4940352c9d4
ohinkkan <otto.hinkkanen@helsinki.fi>
2014-09-30 15:53:05 +0300

    direct json transfer instead of written file yo

--- zab2.go
+++ zab2.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"html/template"
 	"bufio"
+	"bytes"
 	"os"
 	"encoding/json"
 	"fmt"
@@ -17,16 +18,43 @@ import (
 // non-empty ids: 23672, 23690, 23685, 23715, 23710
 
 var db sql.DB 
+var id int
+var id2 string
 
 func handler(w http.ResponseWriter, r *http.Request) {
     url := r.URL.Path
     if (strings.Contains(url, ":")) {
-        fmt.Printf("\nurl=%v",url)
-        ids := strings.Split(url, ":")
-        fmt.Printf("\nid=%v",ids[1])
-        fmt.Printf("\nid2=%v",ids[2])
-//        fmt.Printf("\nid2=%v",ids[2])
-        id, err := strconv.Atoi(ids[1])
+    	fmt.Printf("\nurl=%v",url)
+		ids := strings.Split(url, ":")
+      	idT, err := strconv.Atoi(ids[1])
+      	if (err == nil) {
+        	id = idT
+        } else {
+        	id = 0
+        }
+      	if (len(ids) > 2) {
+			id2 = ids[2]
+		} else {
+			id2 = ""
+		}
+		fmt.Printf("\nid=%v",id)
+		fmt.Printf("\nid2=%v",id2)
+    } else {
+    	id = 0
+    	id2 = ""
+    }
+	t, _ := template.ParseFiles("zab.html")
+	t.Execute(w, "testi")
+}
+
+func handler1(w http.ResponseWriter, r *http.Request) {
+    url := r.URL.Path
+    if (strings.Contains(url, ":")) {
+      fmt.Printf("\nurl=%v",url)
+      ids := strings.Split(url, ":")
+      fmt.Printf("\nid=%v",ids[1])
+      fmt.Printf("\nid2=%v",ids[2])
+       id, err := strconv.Atoi(ids[1])
         
 //        id2, err2 := strconv.Atoi(ids[2])
         if (err == nil) {
@@ -41,6 +69,30 @@ func handler(w http.ResponseWriter, r *http.Request) {
     //}
 }
 
+func dhandler(w http.ResponseWriter, r *http.Request) {
+	q := dbquery(id,id2)
+	t := template.New("test")
+	t, _ = t.Parse(q)
+	t.Execute(w, "testi")
+}
+
+func dhandler1(w http.ResponseWriter, r *http.Request) {
+	url := r.URL.Path
+    if (strings.Contains(url, ":")) {
+        fmt.Printf("\nurl=%v",url)
+        ids := strings.Split(url, ":")
+        fmt.Printf("\nid=%v",ids[1])
+        fmt.Printf("\nid2=%v",ids[2])
+        id, err := strconv.Atoi(ids[1])
+        if (err == nil) {
+        	q := dbquery(id,ids[2])
+        	t := template.New("test")
+   			t, _ = t.Parse(q)
+   			t.Execute(w, "testi")
+        }
+    } 
+}
+
 func ghandler(w http.ResponseWriter, r *http.Request) {
     t, _ := template.ParseFiles("graph.js")
     t.Execute(w, "testi")
@@ -51,17 +103,13 @@ func bhandler(w http.ResponseWriter, r *http.Request) {
     t.Execute(w, "testi")
 }
 
-func dhandler(w http.ResponseWriter, r *http.Request) {
-    t, _ := template.ParseFiles("data_sample.txt")
-    t.Execute(w, "testi")
-}
 
 type TestZab struct {
-	Clock int64	`json:"time"`
+	Clock int64		`json:"time"`
 	Value float32	`json:"val"`
 }
 
-func dbquery(id int, id2 string) {
+func dbquery1(id int, id2 string) {
 
 	db, err := sql.Open("postgres", "user=ohtu dbname=multi-axis sslmode=disable")
 	if err != nil {
@@ -124,6 +172,41 @@ func dbquery(id int, id2 string) {
  	
 	if err != nil { panic(err) }	
 }
+
+func dbquery (id int, id2 string) string {
+	db, err := sql.Open("postgres", "user=ohtu dbname=multi-axis sslmode=disable")
+	if err != nil {
+		log.Fatal(err)
+	}
+	rows, err := db.Query(`SELECT clock, value FROM history where history.itemid IN (SELECT itemid FROM items WHERE hostid = $1 and key_ = $2) ORDER BY clock`, id, id2)
+	if err != nil {
+		log.Fatal(err)
+    }
+//	ensure rows are closed
+	defer rows.Close()
+	
+	var buffer bytes.Buffer 
+    var value float32
+	var clock int64
+    first := true;
+    buffer.WriteString("[")
+	for rows.Next() {
+		if err := rows.Scan(&clock,&value); err != nil {
+                    log.Fatal(err)
+          	}
+          	m := TestZab{clock,value}
+          	b, err := json.Marshal(m)
+       		if err != nil { panic(err) }
+       		if (!first) {
+	          	buffer.WriteString(",")
+       		}
+        	buffer.Write(b) 
+        	first = false;  	
+    	}
+	buffer.WriteString("]")
+	return buffer.String()
+}
+
 
 func main() {
 	http.HandleFunc("/data_sample.txt", dhandler)	
