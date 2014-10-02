3df3ae40e83dcb97bc6e73d2eab57d23fb502b88
Antti <antti.karikoski@cs.helsinki.fi>
2014-10-02 17:10:10 +0300

    metric data visualisation with history and future

--- graph.js
+++ graph.js
@@ -1,50 +1,66 @@
-var allText = $.getJSON("http://localhost:8080/data_sample.txt", function(data) {
-	draw(data);
-});
+var width = 1200;
+var height = 700;
+var svgContainer = d3.select("div").append("svg:svg").attr("width", width).attr("height", height);
+var allData;
 
-function draw(chartData) {
-    var width = 800;
-    var height = 700;
+$.getJSON(window.location.href, function(data) {
+	draw(data.history, data.future, svgContainer);
+    allData = data;
+    // console.log(JSON.stringify(allData))
+})
+
+function draw(leftGrapHistoryhData, leftGraphFutureData, svgContainer) {
+    
     var margin = {"left": 50, "right":20, "top": 30, "bottom":30};    
+    var today = (width * (2/3) ) - margin.right;
 
-    var xMin = d3.min(chartData, function(d){return Math.min(d.time)});
-    var xMax = d3.max(chartData, function(d){return Math.max(d.time)});
+    var historyXMin = d3.min(leftGrapHistoryhData, function(d){return Math.min(d.time)});
+    var historyXMax = d3.max(leftGrapHistoryhData, function(d){return Math.max(d.time)});
+    var historyYMin = d3.min(leftGrapHistoryhData, function(d){return Math.min(d.val)});
+    var historyYMax = d3.max(leftGrapHistoryhData, function(d){return Math.max(d.val)});
+    var historyXScale = d3.time.scale().domain([historyXMin, historyXMax]).range([margin.left, today]);
 
-    var yMin = d3.min(chartData, function(d){return Math.min(d.val)});
-    var yMax = d3.max(chartData, function(d){return Math.max(d.val)});
+    var futureYMin = d3.min(leftGraphFutureData, function(d){return Math.min(d.val)});
+    var futureYMax = d3.max(leftGraphFutureData, function(d){return Math.max(d.val)});
+    var futureXMin = d3.min(leftGraphFutureData, function(d){return Math.min(d.time)});
+    var futureXMax = d3.max(leftGraphFutureData, function(d){return Math.max(d.time)});
+    var futureXScale = d3.time.scale().domain([futureXMin, futureXMax]).range([today, width - margin.right]);
 
-    console.log(" yMin: " + yMin + " ymax: " + yMax + " " + chartData.length);
-	
-    var xScale = d3.time.scale().domain([xMin, xMax]).range([margin.left, width - margin.right]);
+    var yMin = Math.min(historyYMin, futureYMin);
+    var yMax = (Math.max(historyYMax, futureYMax));
+    var yScale = d3.scale.linear().domain([(1.5*yMax),(yMin-30)]).range([0,height+margin.top])
 
-    var xAxisScale = d3.time.scale().domain([xMin,xMax]).range([margin.left,width]);
-    var yScale = d3.scale.linear().domain([(1.5*yMax),-3]).range([0,height+margin.top])
+    var historyGraphFunc = d3.svg.line()
+                                .x(function(d, i) {return historyXScale(d.time)})
+                                .y(function(d) {return yScale(d.val) })
+                                .interpolate("none");
 
-    var valueFormat = (function(d) {return yScale(d.val) });
-
-    var lineFunc = d3.svg.line()
-                        .x(function(d, i) {return xScale(d.time)})
-                        .y(valueFormat)
-                        .interpolate("none");
-
-    var svgContainer = d3.select("div").append("svg:svg").attr("width", width).attr("height", height).append("svg:g");
+    var futureGraphFunc = d3.svg.line()
+                                .x(function(d, i) {return futureXScale(d.time)})
+                                .y(function(d) {return yScale(d.val) })
+                                .interpolate("none");
 
     
-    svgContainer.append("svg:path").attr("d",lineFunc(chartData)).attr("stroke","blue").attr("fill","none");
-
+    svgContainer.append("svg:path").attr("d", historyGraphFunc(leftGrapHistoryhData)).attr("stroke","blue").attr("fill","none");
+    svgContainer.append("svg:path").attr("d", futureGraphFunc(leftGraphFutureData)).attr("stroke","red").attr("fill","none")
 
     var chartXAxis = d3.svg.axis()
-                          .scale(xAxisScale)
+                          .scale(historyXScale)
                           .orient("bottom")
-                          .ticks(5)
+                          .ticks(3)
                           .tickFormat(function(d) { return d3.time.format('%Y %b %d, %H : %M')(new Date(d)); });
 
+    var futureXAxis = d3.svg.axis()
+                          .scale(futureXScale)
+                          .orient("bottom")
+                          .ticks(3)
+                          .tickFormat(function(d) { return d3.time.format('%b %d, %H:%M')(new Date(d)); });
 
     var chartYAxis = d3.svg.axis()
                           .scale(yScale)
                           .orient("left")
                           .ticks(6)
-                          .tickFormat(function(d) {return (d) + " MB"});
+                          .tickFormat(function(d) {return (d)});
 
     svgContainer.append("g")
                 .attr("class", "axis")
@@ -54,4 +70,8 @@ function draw(chartData) {
                 .attr("class", "axis")
                 .attr("transform", "translate(0," + (height-margin.bottom) + ")")
                 .call(chartXAxis);
+    svgContainer.append("g")
+                .attr("class", "futureAxis")
+                .attr("transform", "translate(" + 0 + "," + (height-margin.bottom) + ")")
+                .call(futureXAxis);
 }
