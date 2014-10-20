53ee99722a4ae82066a2709c829514c479762a7b
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2014-10-20 12:18:10 +0300

    Simplified DummyApp.

--- forecast-java/src/main/java/com/github/multi_axis/DummyApp.java
+++ forecast-java/src/main/java/com/github/multi_axis/DummyApp.java
@@ -11,23 +11,16 @@ import static com.github.multi_axis.ForecastProcess.forecastAlts2;
 
 public class DummyApp {
 
-  private IO<Unit> 
-    app() { return  forecastAlts2(System.in,
-                      System.out,
-                      Conf.Reader.zabReader,
-                      Conf.Writer.zab0Writer,
-                      Conf.Writer.zab3Writer,
-                      (x  -> x),
-                      (x  -> x)); }
+  private static IO<Unit> 
+    app = forecastAlts2(System.in,
+                        System.out,
+                        Conf.Reader.zabReader,
+                        Conf.Writer.zab0Writer,
+                        Conf.Writer.zab3Writer,
+                        (x  -> x),
+                        (x  -> x));
 
-  // public static void main(String[] args) throws IOException { app().run(); }
   public static void main(String[] args) throws IOException {
-    forecastAlts2(System.in,
-                  System.out,
-                  Conf.Reader.zabReader,
-                  Conf.Writer.zab0Writer,
-                  Conf.Writer.zab3Writer,
-                  (x  -> x),
-                  (x  -> x)).run(); }
+    app.run(); }
 
 }
