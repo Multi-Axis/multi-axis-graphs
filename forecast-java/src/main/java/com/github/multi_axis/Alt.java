7e5095ca0c2676c817e524b63b101f377f86c978
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2014-11-11 08:47:51 +0200

    First draft of Alt.

---
+++ forecast-java/src/main/java/com/github/multi_axis/Alt.java
@@ -0,0 +1,56 @@
+package com.github.multi_axis;
+
+//  - TODO THINK Recall -- what is the role for this and is it actually
+//    necessary?
+//    - This vs. plain 'Alt', that is.
+//    - That which I am trying to do, could I achieve simply by having
+//      properly limited constructors in one unified class?
+
+public abstract class Alt<OTHERS,TAG,VAL> {
+
+  public abstract <R> R 
+    match(F<OTHERS,R> othersfun,
+          TAG tag,
+          F<VAL,R> fun);
+
+  public static <OTHERS,TAG,VAL> Alt<OTHERS,TAG,VAL>
+    other(OTHERS others) {
+      return new Other<OTHERS,TAG,VAL>(other); }
+
+  public static <OTHERS,TAG,VAL> Alt<OTHERS,TAG,VAL>
+    val(TAG tag, VAL val) {
+      return new Val<OTHERS,TAG,VAL>(val); }
+
+  private static final class Other<OTHERS,TAG,VAL> 
+    extends Alt<OTHERS,TAG,VAL> {
+
+    private OTHERS other;
+
+    public <R> R
+      match(F<OTHERS,R> othersfun,
+            TAG tag,
+            F<VAL,R>    fun) {
+        return othersfun.f(other); }
+
+    public Other(OTHERS other) {
+      this.other = other; }
+  }
+
+  private static final class Val<OTHERS,TAG,VAL>
+    extends Alt<OTHERS,TAG,VAL> {
+
+    private VAL val;
+
+    public <R> R
+      match(F<OTHERS,R> othersfun,
+            TAG tag,
+            F<VAL,R>    fun) {
+        return fun.f(val); }
+
+    public Val(VAL val) {
+      this.val = val; }
+  }
+
+
+  private Alt() {}
+}
