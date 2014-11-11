098447c48db07f9e67905cc318f4be764dc2eeec
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2014-11-11 09:03:33 +0200

    Added return type declaration to nothingFunction.f

--- forecast-java/src/main/java/com/github/multi_axis/Nothing.java
+++ forecast-java/src/main/java/com/github/multi_axis/Nothing.java
@@ -8,7 +8,7 @@ public abstract class Nothing {
 
   public static <R> F<Nothing,R> nothingFunction() {
     return new F<Nothing,R>() {
-      public f(Nothing nothing) { throw new NothingAppliedException(); } } }
+      public R f(Nothing nothing) { throw new NothingAppliedException(); } } }
 
   private Nothing() {}
 
