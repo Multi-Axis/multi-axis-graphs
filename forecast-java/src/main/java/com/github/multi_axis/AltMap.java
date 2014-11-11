22ec31f96d971d68af215101e32cfa3631982b27
Timo Sinnemäki <timo.sinnemaki@helsinki.fi>
2014-11-11 08:28:31 +0200

    First draft of AltFun and AltMap.

---
+++ forecast-java/src/main/java/com/github/multi_axis/AltMap.java
@@ -0,0 +1,37 @@
+package com.github.multi_axis;
+
+public final class AltMap<A,B> {
+
+  public final F<A,B> fun;
+
+  public <INTAG,IN,OUTTAG,OUT> AltFun<Alt<A,INTAG,IN>, Alt<B,OUTTAG,OUT>>
+    add(final INTAG intag, final OUTTAG outtag, final F<IN,OUT> f) {
+      return altMap(this,intag,outtag,f); } 
+  
+  public static <INS,INTAG,IN,OUTS,OUTTAG,OUT> AltMap<Alt<INS,INTAG,IN>,
+                                                      Alt<OUTS,OUTTAG,OUT>>
+    altMap( final AltMap<INS,OUTS> othersmap,
+            final INTAG intag,
+            final OUTTAG outtag,
+            final F<IN,OUT> fun) {
+      return makeAltMap(othersmap.fun, intag, outtag, fun); }
+
+  public static <INTAG,IN,OUTTAG,OUT> AltMap< Alt<Nothing,INTAG,IN>,
+                                              Alt<Nothing,OUTTAG,OUT>>
+    altMap(final INTAG intag, final OUTTAG outtag, final F<IN,OUT> fun) {
+      return makeAltMap(Nothing.<Nothing>nothingFunction(),
+                        intag, outtag, fun); }
+
+  private static <INS,INTAG,IN,OUTS,OUTTAG,OUT> AltMap< Alt<INS,INTAG,IN>,
+                                                        Alt<OUTS,OUTTAG,OUT>>
+    makeAltMap( final F<INS,OUTS> othersfun,
+                final INTAG intag,
+                final OUTTAG outtag,
+                final F<IN,OUT> fun) {
+      return new AltMap<Alt<INS,INTAG,IN>,Alt<OUTS,OUTTAG,OUT>>(alt  ->
+          alt.match(
+            others  -> Alt.other(othersfun.fun.f(others)),
+            intag,
+            in     -> Alt.val(outtag, fun.f(in)))); }
+
+
