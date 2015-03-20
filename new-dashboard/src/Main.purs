d03e927b66640907055129a52524dede405773d2
Samuli Thomasson <samuli.thomasson@relex.fi>
2015-03-20 14:00:44 +0200

    tunk new dashboard ui

--- new-dashboard/src/Main.purs
+++ new-dashboard/src/Main.purs
@@ -8,17 +8,18 @@ import Data.Either
 import Data.Maybe
 import Data.Foldable
 import Data.List (List())
-import Data.Array
 import DOM
+import Data.Foreign.Undefined
 
-import Text.Smolder.HTML (h1)
+import Text.Smolder.HTML (table, tr, th, td, a, h2, div)
+import Text.Smolder.HTML.Attributes (href, className, title)
 import Text.Smolder.Markup
 import Text.Smolder.Renderer.String (render)
 
 import Debug.Trace
 
 main = do
-   runContT getCont print
+   runContT getCont trace
 
 getCont = do
   res <- ajaxCont "habbix/dashboard" $ HttpRequest {accepts: Json, contentType: TextContent, method: GET, contents: Nothing}
@@ -30,28 +31,71 @@ getCont = do
         return ""
 
 type Host = { hostname :: String, hostid :: Number, items :: [Item] }
-
--- instance hostIsForeign :: IsForeign Host where
---     read value = do
---         h   <- readProp "hostname" value
---         hid <- readProp "hostid" value
---         return $ Host { hostname : h, hostid : hid, items : [] }
--- 
-type Item = 
-          { metric_name :: String
-          , current_value :: Number
-          , current_time :: Number
-          , next24h :: Number
-          , next6d :: Number
-          , past7d :: Number
-          }
+type Item = { metric_name :: String, metric_scale :: Number
+            , current_value :: Number, current_time :: Number
+            , next24h :: Number, next6d :: Number, past7d :: Number
+            , threshold_lower :: Boolean
+            , threshold_high :: Number, threshold_warning :: Number, threshold_critical :: Number }
 
 dashboardView :: [Host] -> Markup
-dashboardView hosts = do
-    for_ hosts $ \host -> do
-        h1 $ text (host.hostname)
+dashboardView hosts =
+    div (for_ hosts $ \h -> div (hostView h) ! className "host-block") ! className "new-dashboard"
+  where
+    hostView host = do
+        h2 $ text (host.hostname)
+        table $ do
+            -- tr $ do
+            --     th (text "")
+            --     th (text "past 7d")
+            --     th (text "now")
+            --     th (text "next 24h")
+            --     th (text "next 6d")
+            for_ (host.items) (tr <<< itemView host.hostname)
+
+itemView :: String -> Item -> Markup
+itemView hn i
+    | i.current_time < 0 = do
+        td $ text i.metric_name
+        td $ text "(n/a)"
+    | otherwise          = do
+
+        th $ a (text i.metric_name)
+            ! href ("/item/" <> hn <> "/" <> i.metric_name)
+            ! title ("threshold (" <> (if i.threshold_lower then "_): " else "^): ")
+                    <> metricPP i i.threshold_critical <> "/" <> metricPP i i.threshold_warning <> "/" <> metricPP i i.threshold_high
+                    <> ", scale " <> show i.metric_scale)
+
+        let pp v = td (text (metricPP i v)) ! className (thresholdClassName i v)
+        pp i.past7d
+        pp i.current_value
+        pp i.next24h
+        pp i.next6d
+
+metricPP :: Item -> Number -> String
+metricPP i v = show $ sigFigs (v / i.metric_scale) 2
+
+thresholdClassName :: Item -> Number -> String
+thresholdClassName i n = go
+    where cmp v t                         = if i.threshold_lower then v <= t else v >= t
+          go | cmp n i.threshold_critical = "critical"
+             | cmp n i.threshold_high     = "high"
+             | cmp n i.threshold_warning  = "warning"
+             | otherwise                  = "normal"
+
 
 foreign import asDashboard
     "function asDashboard(html) {\
-    \   document.getElementById(\"dashboard\").innerHTML = html;\
+    \   return function(){\
+    \      document.getElementById(\"dashboard\").innerHTML = html;\
+    \   }\
     \}" :: forall eff. String -> Eff (dom :: DOM | eff) Unit
+
+foreign import sigFigs
+    "function sigFigs(n) {\
+    \   return function(sig) {\
+    \     if (n == 0) return 0;\
+    \     var mult = Math.pow(10, sig - Math.floor(Math.log(Math.abs(n)) / Math.LN10) - 1);\
+    \     console.log(n, sig, mult);\
+    \     return Math.round(n * mult) / mult;\
+    \   }\
+    \}" :: Number -> Number -> Number
